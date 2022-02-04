b434f2a49ffeb1703a639aa4151ec03237d3e9ac
MichalBielicki <80898561+MichalBielicki@users.noreply.github.com>
2022-02-04 14:24:39 +0100

    Validation fixed

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 import modalToggle from "./modalToggle.js";
 
 const input = document.querySelector(".input");
+const inputKey = document.querySelector(".input-key");
 const button = document.querySelector(".btn");
 
 button.disabled = true;
 
-input.addEventListener("change", stateHandle);
 button.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (isNaN(input.value)) {
+    alert("Numer telefonu musi się składać z cyfr.");
+    return false;
+  }
+  if (isNaN(inputKey.value)) {
+    alert("Kod musi się składać z cyfr.");
+    return false;
+  }
   modalToggle(e);
-  c.start();
 });
 
+input.addEventListener("input", stateHandle);
+inputKey.addEventListener("input", stateHandle);
+
 function stateHandle() {
-  if (document.querySelector(".input").value === "") {
-    button.disabled = true;
-  } else {
+  if (input.value.length === 9 && inputKey.value.length === 4) {
     button.disabled = false;
+  } else {
+    button.disabled = true;
   }
 }
